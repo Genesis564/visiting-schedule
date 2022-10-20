20217aafb16db7298294b071bb757b1540037bda
Genesis564 <petrikeev.egor03@mail.ru>
2022-10-21 02:38:17 +0500

    Изменение шаблонизатора на Freemaker и добавление списка пользователей с возможностью его редактирования

--- src/main/java/com/okei/visitingschedule/configuration/MvcConfig.java
+++ src/main/java/com/okei/visitingschedule/configuration/MvcConfig.java
@@ -1,8 +1,11 @@
 package com.okei.visitingschedule.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
