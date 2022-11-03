a6974b39a7be072c29e91314b565176e15fad49d
Genesis564 <petrikeev.egor03@mail.ru>
2022-11-03 09:43:26 +0500

    Публикация приложения на сервере Nginx, находящимся на виртуальной машине.

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+mvn clean package
+
+echo 'Copy files...'
+
+scp -i ~/.ssh/id_rsa \
+    target/hibernate-test-1.0-SNAPSHOT.jar \
+    su@192.168.147.73:/home/su/
+
+echo 'Restart server...'
+
+ssh -i ~/.ssh/id_rsa su@192.168.147.73 << EOF
+pgrep java | xargs kill -9
+nohup java -jar hibernate-test-1.0-SNAPSHOT.jar > log.txt &
+EOF
+
+echo 'Complete'
